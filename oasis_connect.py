a74c4becf8b211a0aab97cd06702d3c4b11b219c
ryanflahertyOG <31020462+ryanflahertyOG@users.noreply.github.com>
2017-12-21 14:54:23 -0800

    Update oasis_connect.py

--- oasis_connect.py
+++ oasis_connect.py
@@ -5,18 +5,22 @@ import requests
 import lxml
 import zipfile
 import io
+import glob
+import os
 
-params = {'queryname': PRC_LMP, 'startdatetime': 20130919T07: 00 - 0000,
-          'enddatetime': 20130920T07: 00 - 0000, 'market_run_id': DAM, 'version': 1,
-          'grp_type': ALL_APNODES}
-source = requests.get(
-    '''http://oasis.caiso.com/oasisapi/SingleZip?queryname=PRC_LMP&startdatetime=20130919T07:00-0000&enddatetime=20130920T07:00-0000&version=1&market_run_id=DAM&node=LAPLMG1_7_B2''')
-
-html = source.text
+url = "http://oasis.caiso.com/oasisapi/SingleZip"
+params = {'queryname': ' PRC_LMP', 'startdatetime': ' 20130919T07: 00 - 0000',
+          'enddatetime': '20130920T07: 00 - 0000', 'market_run_id': ' DAM', 'version': 1,
+          'grp_type': 'ALL_APNODES'}
+source = requests.get(url, params)
 content = source.content
 z = zipfile.ZipFile(io.BytesIO(source.content))
 z.extractall()
+f = glob.glob('*.xml')
+print(f[0])
+print(str(f[0]))
 
-with open("20130919_20130920_PRC_LMP_DAM_20171220_21_19_29_v1.xml") as test:
+with open(str(f[0])) as test:
     soup = BeautifulSoup(test, 'lxml')
 print(soup.prettify())
+20130919_20130920_PRC_LMP_DAM_20171220_21_19_29_v1.xml
