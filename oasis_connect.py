267ea3224c5e538ca4c4f41e3cd0c2f9138efee4
ryanflahertyOG <31020462+ryanflahertyOG@users.noreply.github.com>
2017-12-22 18:03:43 -0800

    Add files via upload

--- oasis_connect.py
+++ oasis_connect.py
@@ -1,26 +1,23 @@
 """Connect to Oasis API and pull data"""
 
-from bs4 import BeautifulSoup
 import requests
-import lxml
 import zipfile
 import io
 import glob
 import os
+import pandas as pd
 
 url = "http://oasis.caiso.com/oasisapi/SingleZip"
-params = {'queryname': ' PRC_LMP', 'startdatetime': ' 20130919T07: 00 - 0000',
-          'enddatetime': '20130920T07: 00 - 0000', 'market_run_id': ' DAM', 'version': 1,
-          'grp_type': 'ALL_APNODES'}
+params = {'queryname': 'PRC_INTVL_LMP', 'version': 3, 'resultformat': 6,
+          'startdatetime': '20171221T08:00-0000',
+          'enddatetime': '20171222T08:00-0000', 'market_run_id': 'RTM',
+          'node': 'ELAP_PGE-APND'}
 source = requests.get(url, params)
 content = source.content
 z = zipfile.ZipFile(io.BytesIO(source.content))
 z.extractall()
-f = glob.glob('*.xml')
-print(f[0])
-print(str(f[0]))
+f = glob.glob('*.csv')
 
-with open(str(f[0])) as test:
-    soup = BeautifulSoup(test, 'lxml')
-print(soup.prettify())
-20130919_20130920_PRC_LMP_DAM_20171220_21_19_29_v1.xml
+print(f[0])
+
+raw = pd.read_csv(f[0])
